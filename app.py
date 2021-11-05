3e9739070bbc65a4588ce40ba64d4b672a512286
Paul Hicks <contactpaulhicks@gmail.com>
2021-11-05 16:28:20 -0400

    Added route for Fastest Eco QP/Ranked leaderboards

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 from bigquery import create_client, run_query
 
-
 app = Flask(__name__)
 
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
@@ -20,10 +19,11 @@ BROWSERS = [
 
 app.jinja_env.globals['current_year'] = datetime.utcnow().year
 app.jinja_env.globals['nav_links'] = [
-        {'name': 'Home', 'url': 'main'},
-        {'name': 'Queen Beans', 'url': 'queen_beans'},
-        {'name': 'Total Set Beans', 'url': 'total_set_beans'},
-    ]
+    {'name': 'Home', 'url': 'main'},
+    {'name': 'Queen Beans', 'url': 'queen_beans'},
+    {'name': 'Total Set Beans', 'url': 'total_set_beans'},
+    {'name': 'Fastest Eco - QP/Ranked', 'url': 'fastest_eco_qp_ranked'},
+]
 
 MAP_IMAGES = [
     'the-black-queens-keep.png',
@@ -69,7 +69,6 @@ def queen_beans():
         map_image = re.sub('[^a-z0-9 ]+', '', map_name.lower()).replace(' ', '-') + '.png'
         player_stats = []
         for player_entry in row['player_stats']:
-
             player_stats.append({
                 'metric': player_entry['totalBerryDeposits'],
                 'player': player_entry['nickname'],
@@ -84,6 +83,51 @@ def queen_beans():
 
     if request.user_agent.browser in BROWSERS:
         return render_template('solo-leaderboards.jinja2', leaderboard_title='Queen Bean',
+                               leaderboard_items=leaderboard_items)
+    else:
+        return jsonify(leaderboard_items)
+
+
+@app.route('/fastest-eco-qp-ranked')
+def fastest_eco_qp_ranked():
+    bigquery_client = create_client()
+    leaderboard_items = []
+    query_results = run_query('bigquery/queries/ranked_qp_fastest_eco.sql', bigquery_client)
+
+    for row in query_results:
+
+        map_name = row['map']
+        map_image = re.sub('[^a-z0-9 ]+', '', map_name.lower()).replace(' ', '-') + '.png'
+        player_stats = []
+        for match_entry in row['match_stats']:
+            ind_player_stats = []
+            for player_stat in match_entry['player']:
+                ind_player_stats.append(
+                    {
+                        'player': player_stat['nickname'],
+                        'individual_metric': '{:d} beans'.format(player_stat['totalBerryDeposits']),
+                        'role': player_stat['role']
+                    }
+                )
+
+            player_stats.append(
+                {
+                    'metric': '{:.3f}s'.format(match_entry['duration']),
+                    'ts': match_entry['matchTimestamp'].isoformat(),
+                    'players': ind_player_stats
+                }
+            )
+
+        leaderboard_items.append(
+            {
+                'content_title': map_name,
+                'map_image': url_for('static', filename=f'maps/{map_image}'),
+                'leaderboard_entries': player_stats
+            }
+        )
+
+    if request.user_agent.browser in BROWSERS:
+        return render_template('team-leaderboards.jinja2', leaderboard_title='Fastest Eco - QP/Ranked',
                                leaderboard_items=leaderboard_items)
     else:
         return jsonify(leaderboard_items)
