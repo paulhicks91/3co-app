b11e9047acf71a9d0556c260b05f711e1374458b
paulh <contactpaulhicks@gmail.com>
2022-02-04 13:05:33 -0800

    Made queen_bean_rankings table in big_query to increase response time.

--- bigquery/queries/queen_bean_rankings.sql
+++ bigquery/queries/queen_bean_rankings.sql
@@ -1,41 +1,6 @@
-WITH stat_agg AS (
-    SELECT
-        map,
-        totalBerryDeposits,
-        nickname,
-        MIN(matchTimestamp) AS matchTimestamp
-    FROM `hot-chee-to.match_stats.game_berry_stats`
-    WHERE playerId IS NOT NULL
-    AND role='Queen'
-    AND match_type IN ('Quick Play', 'Ranked')
-    AND totalBerryDeposits > 0
-    AND bot_player != 'Bot'
-    GROUP BY 1,2,3
-), new_agg AS (
-    SELECT
-        map,
-        totalBerryDeposits,
-        nickname,
-        matchTimestamp
-    FROM stat_agg
-    RIGHT JOIN (
-        SELECT
-            map,
-            MAX(totalBerryDeposits) AS totalBerryDeposits,
-            nickname
-        FROM stat_agg
-        GROUP BY 1,3
-    ) USING (map, totalBerryDeposits, nickname)
-    WHERE matchTimestamp IS NOT NULL
-)
-
 SELECT
     map,
-    ARRAY_AGG(STRUCT(
-    totalBerryDeposits,
-    nickname,
-    matchTimestamp) ORDER BY totalBerryDeposits DESC, matchTimestamp, nickname LIMIT 20) AS player_stats
-FROM new_agg
-GROUP BY 1
+    player_stats
+FROM `hot-chee-to.match_stats.queen_bean_leaderboards`
 ORDER BY map
 ;
